9defcbd7194844d38e95f9a22551edaf48d68ae6
채승지 <cotmdwl1405@gmail.com>
2023-11-06 13:20:24 +0900

    first commit

---
+++ back-end/yaedora/src/test/java/com/yaedora/YaedoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaedora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaedoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
