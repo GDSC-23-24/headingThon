b4a4db9432244969572553f8d7fca411ada675bc
채승지 <cotmdwl1405@gmail.com>
2023-11-06 20:54:47 +0900

    feat : location 개발

---
+++ back-end/yaedora/src/main/java/com/yaedora/Location/LocationRepository.java
@@ -0,0 +1,10 @@
+package com.yaedora.Location;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LocationRepository extends JpaRepository<Location,Long> {
+
+
+}
---
+++ back-end/yaedora/src/main/java/com/yaedora/Location/TownDto.java
@@ -0,0 +1,10 @@
+package com.yaedora.Location;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TownDto {
+    private String town;
+}
