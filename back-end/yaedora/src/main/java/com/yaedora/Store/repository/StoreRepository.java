26838d24f95986e7477017beda61bbd8c1cd5f95
채승지 <cotmdwl1405@gmail.com>
2023-11-07 23:17:21 +0900

    feat : 좋아요 기능 개발

--- back-end/yaedora/src/main/java/com/yaedora/Store/repository/StoreRepository.java
+++ back-end/yaedora/src/main/java/com/yaedora/Store/repository/StoreRepository.java
@@ -3,6 +3,13 @@ package com.yaedora.Store.repository;
 
 import com.yaedora.Store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StoreRepository extends JpaRepository<Store,Long> {
+
+    @Query("select s from Store s where s.id = :id")
+    List<Store> findStoresById(@Param("id") Long id);
 }
