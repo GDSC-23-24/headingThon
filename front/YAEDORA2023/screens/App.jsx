65a3d02fec9352801bc0bda45cdfead83f64b8de
rzl0zi <program.yunji@gmail.com>
2023-11-08 05:33:47 +0900

    수정, notice, my, main프론트 개발

--- front/YAEDORA2023/screens/App.jsx
+++ front/YAEDORA2023/screens/App.jsx
@@ -3,6 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Start from './Start';
 import Main from './Main'
+import Splash from './Splash';
+import Notice from './Notice';
+import My from './My';
+
 ;
 
 const Stack = createNativeStackNavigator();
@@ -10,10 +14,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
-      
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
         <Stack.Screen name="Start" component={Start} options={{ headerShown: false }} />
         <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
+        <Stack.Screen name="Notice" component={Notice} options={{ headerShown: false }} />
+        <Stack.Screen name="My" component={My} options={{ headerShown: false }} />
 
       </Stack.Navigator>
     </NavigationContainer>
