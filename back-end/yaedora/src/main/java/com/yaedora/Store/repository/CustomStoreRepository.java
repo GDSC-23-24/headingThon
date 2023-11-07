4195142862d340e7364c0d0fb92ee7497b8f5bea
채승지 <cotmdwl1405@gmail.com>
2023-11-07 23:17:21 +0900

    feat : 좋아요 기능 개발

---
+++ back-end/yaedora/src/main/java/com/yaedora/Store/repository/CustomStoreRepository.java
@@ -0,0 +1,12 @@
+package com.yaedora.Store.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomStoreRepository {
+    private final EntityManager em;
+//    private final
+}
